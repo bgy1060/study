90972b266d8b8c2023a34bd7321a98abe7ba7920
박지연 <bgy1060@naver.com>
2020-05-23 12:21:18 +0900

    Update CYK_algorithm.py

--- CYK_algorithm.py
+++ CYK_algorithm.py
@@ -1,10 +1,16 @@
-########################################### # cyk algorithm ###########################################
+###########################################
+# cyk algorithm
+###########################################
+
 import sys
+
 ### save production rule ###
+
 ruleMap = {} # empty dictionary
 
+print("---Context-Free Grammar Test---")
 ruleCnt = int(input('규칙의 개수: '))
-print('규칙: ')
+print('규칙을 입력하세요: ')
 
 for i in range(ruleCnt):
     instr = input()
@@ -17,14 +23,15 @@ for i in range(ruleCnt):
             ruleMap[key] = lst
         else: # in list
             lst.append(str[0])
-        else:
-            print('input error!!!') # input error handling
-            sys.exit()
+    else:
+        print('input error!!!') # input error handling
+        sys.exit()
             
 ### parse into substring ###
 parseVarMap = {} # empty dictionary
 w = input('멤버 확인이 필요한 문자열: ')
 wlen = len(w)
+
 for leng in range(1, wlen+1):
     for i, j in zip(range(1, wlen-leng+2), range(leng, wlen+1)):
         newkey = (i, j)
@@ -42,27 +49,28 @@ for leng in range(1, wlen+1):
                 
             parseVarMap[newkey] = newVars
             
-         else: # case leng >= 2, union(V(i, k), V(k+1, j))
+        else: # case leng >= 2, union(V(i, k), V(k+1, j))
              for k in range(i, j):
                  key1 = (i, k)
                  key2 = (k+1, j)
                  
-                if not (parseVarMap.get(key1) and parseVarMap.get(key2)):
-                    continue
+                 if not (parseVarMap.get(key1) and parseVarMap.get(key2)):
+                        continue
                 
-                for var1 in parseVarMap.get(key1):
-                    for var2 in parseVarMap.get(key2):
-                        findkey = var1 + var2
-                        ruleVar = ruleMap.get(findkey)
-                        if not ruleVar:
-                            continue
+                 for var1 in parseVarMap.get(key1):
+                     for var2 in parseVarMap.get(key2):
+                         findkey = var1 + var2
+                         ruleVar = ruleMap.get(findkey)
+                         if not ruleVar:
+                             continue
                         
-                        for var in ruleVar:
-                            if not var in newVars: # not in list, append
-                                newVars.append(var)
+                         for var in ruleVar:
+                             if not var in newVars: # not in list, append
+                                 newVars.append(var)
                                 
-                        parseVarMap[newkey] = newVars
-                                    
+                         parseVarMap[newkey] = newVars
+                
+                
 ### check whether 'S' is in the last list ###
 lastkey = (1, wlen)
 lastvars = parseVarMap.get(lastkey)
